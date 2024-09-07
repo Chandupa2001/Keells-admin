53f7a01dc4ba8589cab5c35be58bc1b20d4f8f60
Chandupa2001 <chanduparanawaka2001@gmail.com>
2024-09-08 00:02:03 +0530

    Created items function

--- src/pages/Items/Items.jsx
+++ src/pages/Items/Items.jsx
@@ -1,10 +1,62 @@
-import React from 'react'
-import './Items.css'
+import React, { useEffect, useState } from "react";
+import "./Items.css";
+import { firebase } from "../../configs/FirebaseConfig";
 
 function Items() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, [])
+  
+
+  const fetchData = async () => {
+    try {
+      const querySnapshot = await firebase
+        .firestore()
+        .collection("items")
+        .get();
+      const itemsData = querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setItems(itemsData);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div>Items</div>
-  )
+    <div>
+      <div className="table-container">
+        <h2>Item List</h2>
+        <table className="items-table">
+          <thead>
+            <tr>
+              <th>Serail No</th>
+              <th>Product Name</th>
+              <th>Category</th>
+              <th>Size</th>
+              <th>Unit</th>
+              <th>Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            {items.map((item) => (
+              <tr key={item.id}>
+                <td>{item.serialNo}</td>
+                <td>{item.productName}</td>
+                <td>{item.category}</td>
+                <td>{item.productSize}</td>
+                <td>{item.unit}</td>
+                <td>{item.price}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
 
-export default Items
+export default Items;
